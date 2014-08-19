7b6a6338ebf35a75e6a47598cadb9f4ce50d0fad
Artem Litchmanov <artem@Artems-MacBook-Pro.local>
2014-08-19 13:52:04 -0400

    First commit

---
+++ Assets/COOP Action Game Kit/_Scripts/Sound/SoundGroupManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundGroupManager : MonoBehaviour {
+
+	public AudioClip[] Sounds;
+	public bool Music=false;
+	public bool RandomPitch=false;
+	public float RandomPitchMin,RandomPitchMax;
+	private float startingvolume;
+	private int played=0;
+	
+	void Awake() {
+		startingvolume = audio.volume;
+	}
+	
+	void OnEnable () {
+		if(Sounds.Length==0) {
+			Debug.Log("There is no sound on "+gameObject.name+" SoundGroupManager, ABORT");
+			this.Recycle();
+			return;
+		}
+		if(!audio)
+			gameObject.AddComponent<AudioSource>();
+		PickAudio();
+		if(RandomPitch)
+			audio.pitch = Random.Range(RandomPitchMin,RandomPitchMax);
+		SetVolume();
+		audio.Play();
+		StartCoroutine(WaitForStopPlaying());
+	}
+	
+	public void SetVolume() {
+		if(Music)
+			audio.volume=SoundManager.instance.MusicVolume*startingvolume;
+		else
+			audio.volume=SoundManager.instance.SFXVolume*startingvolume;
+	}
+	
+	void PickAudio() {
+		if(Music) {
+			audio.clip = Sounds[played];
+			if(played<Sounds.Length-1)
+				played++;
+			else
+				played=0;
+		} else {
+			audio.clip = Sounds[Random.Range(0, Sounds.Length)];
+		}
+	}
+	
+	public void ForceStop() {
+		StopAllCoroutines();
+		this.Recycle();
+	}
+	
+	void OnApplicationFocus(bool focus) {
+	   if(!focus) {
+			StopAllCoroutines();
+	    	AudioListener.pause = true;
+	   }
+	   else {
+			AudioListener.pause = false;
+			StartCoroutine(WaitForStopPlaying());
+	   }
+	}
+	
+	IEnumerator WaitForStopPlaying()
+    {
+        while (audio.isPlaying)
+        {
+            yield return 0;
+        }
+        
+        if(SoundManager.instance.currentMusic==this&&this.enabled)
+        	OnEnable();
+        else
+        	this.Recycle();
+    }
+}
---
+++ Assets/COOP Action Game Kit/_Scripts/SpawnMethods/ApplyForceOnSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyForceOnSpawn : MonoBehaviour {
+
+	public bool ApplyRandomDrag=true;
+	public float RandomDragMin=0.4f;
+	public float RandomDragMax=1f;
+	public bool ApplyTorqueY=true;
+	public float RandomTorqueYForce=15f;
+	public float UpRandomForce=4f;
+	public float RandomForce=4f;
+
+	void OnEnable() {
+		if(gameObject.rigidbody) {
+			gameObject.rigidbody.drag = Random.Range(RandomDragMin, RandomDragMax);
+			if(ApplyTorqueY)
+				gameObject.rigidbody.AddRelativeTorque(Vector3.up * Random.Range(-2,2f)*RandomTorqueYForce);
+			gameObject.rigidbody.AddForce(Random.Range(-1,1f)*RandomForce,Random.Range(0,1f)*UpRandomForce,Random.Range(-1,1f)*RandomForce,ForceMode.Impulse);
+		}
+	}
+}
